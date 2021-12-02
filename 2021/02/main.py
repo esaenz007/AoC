5dfbbe1ae48349bd9b4645da90425341384db211
Eduardo Saenz <Eduardo_Saenz2@oxy.com>
2021-12-02 11:07:30 -0600

    Rename some variables

--- 2021/02/main.py
+++ 2021/02/main.py
@@ -5,21 +5,21 @@ directions = open("inputs_test.csv").read().split("\n") # Credit to Alex Lach fo
 
 # Part 1
 # Recursive function returns horz position and depth based on list of directions
-def calc_position(directions:list, curr_depth:int=0, curr_horz_position:int=0, curr_direction=0):
+def calc_position(directions:list, curr_depth:int=0, curr_horz_position:int=0, curr_direction_index=0):
     
-    if curr_direction < len(directions):
-        direction = directions[curr_direction].split(" ")
+    if curr_direction_index < len(directions):
+        curr_direction = directions[curr_direction_index].split(" ")
 
-        if direction[0] == "forward":
-            curr_horz_position += int(direction[1])
-        elif direction[0] == "down":
-            curr_depth += int(direction[1])
-        elif direction[0] == "up":
-            curr_depth -= int(direction[1])
+        if curr_direction[0] == "forward":
+            curr_horz_position += int(curr_direction[1])
+        elif curr_direction[0] == "down":
+            curr_depth += int(curr_direction[1])
+        elif curr_direction[0] == "up":
+            curr_depth -= int(curr_direction[1])
         
-        curr_direction+=1 # Move to next direction
+        curr_direction_index+=1 # Move to next direction
         
-        return calc_position(directions,curr_depth=curr_depth,curr_horz_position=curr_horz_position,curr_direction=curr_direction)
+        return calc_position(directions,curr_depth=curr_depth,curr_horz_position=curr_horz_position,curr_direction_index=curr_direction_index)
     else:
         return curr_depth, curr_horz_position
 
@@ -30,22 +30,22 @@ print(f"Horizontal Position: {horizontal_position}, Depth: {total_depth}, Produc
 
 # Part 2
 # Recursive function returns horz position and depth based on list of directions.  Implements aim.
-def calc_position(directions:list, curr_depth:int=0, curr_horz_position:int=0, curr_aim=0, curr_direction=0):
+def calc_position(directions:list, curr_depth:int=0, curr_horz_position:int=0, curr_aim=0, curr_direction_index=0):
     
-    if curr_direction < len(directions):
-        direction = directions[curr_direction].split(" ")
+    if curr_direction_index < len(directions):
+        curr_direction = directions[curr_direction_index].split(" ")
 
-        if direction[0] == "forward":
-            curr_horz_position += int(direction[1])
-            curr_depth +=  curr_aim * int(direction[1])
-        elif direction[0] == "down":
-            curr_aim += int(direction[1])
-        elif direction[0] == "up":
-            curr_aim -= int(direction[1])
+        if curr_direction[0] == "forward":
+            curr_horz_position += int(curr_direction[1])
+            curr_depth +=  curr_aim * int(curr_direction[1])
+        elif curr_direction[0] == "down":
+            curr_aim += int(curr_direction[1])
+        elif curr_direction[0] == "up":
+            curr_aim -= int(curr_direction[1])
         
-        curr_direction+=1 # Move to next direction
+        curr_direction_index+=1 # Move to next direction
 
-        return calc_position(directions,curr_depth=curr_depth,curr_horz_position=curr_horz_position,curr_aim=curr_aim,curr_direction=curr_direction)
+        return calc_position(directions,curr_depth=curr_depth,curr_horz_position=curr_horz_position,curr_aim=curr_aim,curr_direction_index=curr_direction_index)
     else:
         return curr_horz_position, curr_depth
 
