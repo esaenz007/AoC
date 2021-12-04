99396d213958c21141db69f82bb85dc1b1d718b1
Eduardo Saenz <Eduardo_Saenz2@oxy.com>
2021-12-04 17:47:05 -0600

    Day 4

---
+++ 2021/04/bingo.py
@@ -0,0 +1,63 @@
+class BingoNumber(object):
+    def __init__(self,number:int):
+        self.number:int = number
+        self.marked:bool = False
+    
+
+class BingoCard(object):
+    def __init__(self,numbers):
+        self.numbers = [
+            [
+                BingoNumber(y) for y in x
+            ] for x in numbers
+        ]
+        self.numbers_marked:int = 0
+        self.bingo:bool = False
+
+    def mark(self,number):
+        self.numbers_marked += 1
+        
+        for row_index, row in enumerate(self.numbers):
+            for col_index, n in enumerate(row):
+                if  n.number == number:
+                    n.marked = True
+                    # After 5 marks, start checking for bingo winner
+                    if self.numbers_marked >= 5:
+                        self.bingo = self.check_row(row_index)
+                        if self.bingo:
+                            break
+                        self.bingo = self.check_col(col_index)
+                        if self.bingo:
+                            break
+            if self.bingo:
+                break
+
+        return self.bingo
+
+    def get_unmarked_numbers(self):
+        unmarked_numbers = []
+        temp = [[
+            y.number for y in x if not y.marked
+        ] for x in self.numbers]
+        for x in temp:
+            for y in x:
+                unmarked_numbers.append(y)
+        
+        return unmarked_numbers
+
+    def check_row(self, row_index):
+        return len([x for x in self.numbers[row_index] if x.marked]) == len(self.numbers[row_index])
+
+    def check_col(self,col_index):
+        col = self.get_col(col_index)
+        return len([x for x in col if x.marked]) == len(col)
+
+    def get_col(self, col_index):
+        return [x[col_index] for x in self.numbers]
+
+    def reset_card(self):
+        self.bingo = False
+        self.numbers_marked = 0
+        for x in self.numbers:
+            for y in x:
+                y.marked = False
---
+++ 2021/04/main.py
@@ -0,0 +1,66 @@
+from bingo import BingoCard
+
+input_file_name = "inputs_test.csv"
+numbers_called = []
+bingo_cards = []
+
+# Load input data into memory
+input_data = open(input_file_name).read().split("\n")
+
+# Load called numbers
+numbers_called = [int(x) for x in input_data[0].split(",")]
+
+# Load all bingo cards
+card_index = 2
+while card_index < len(input_data):
+    bingo_card_matrix = [list(filter(None,x.split(" "))) for x in input_data[card_index:card_index+5]]  
+    bingo_card_matrix = [[int(y) for y in x] for x in bingo_card_matrix]
+    bingo_cards.append(BingoCard(bingo_card_matrix))
+    card_index += 6
+
+# Remove input data from memory
+input_data.clear()
+input_data = None
+
+# Part 1
+winning_number = 0
+unmarked_numbers = []
+
+# Play until first card wins
+for n in numbers_called:
+    for bc in bingo_cards:
+        bc.mark(n)
+        if bc.bingo:
+            unmarked_numbers = bc.get_unmarked_numbers()
+            winning_number = n
+            break
+    if bc.bingo: 
+        break
+    
+print("Part 1")
+print("============================")
+print(f"Winning number: {winning_number}")
+print(f"Solution: {winning_number * sum(unmarked_numbers)}")
+
+# Part 2
+# Reset bingo cards
+for x in bingo_cards:
+    x.reset_card()
+
+last_winning_card_index=0
+winning_number=0
+# Play all numbers keeping track of last winning card
+for n in numbers_called:
+    for i, bc in enumerate(bingo_cards):
+        if not bc.bingo: #Only play cards that have not won
+            bc.mark(n)
+            if bc.bingo:
+                last_winning_card_index = i
+                winning_number = n
+
+print("\nPart 2")
+print("============================")
+print(f"Wining number: {winning_number}")
+print(f"Last card to win: {last_winning_card_index + 1}")
+unmarked_numbers = bingo_cards[last_winning_card_index].get_unmarked_numbers()
+print(f"Solution: {winning_number * sum(unmarked_numbers)}")
